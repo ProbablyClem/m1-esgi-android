0dd509e03766e9f6a5012e07430a1474518b2f74
Lastoss <73163448+CooBsCa@users.noreply.github.com>
2023-07-25 18:28:33 +0200

    Add : Avancement Detail Activity

--- app/src/main/java/com/example/pokeapp/DetailActivity.kt
+++ app/src/main/java/com/example/pokeapp/DetailActivity.kt
@@ -1,14 +1,33 @@
 package com.example.pokeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pokeapp.databinding.ActivityDetailBinding
+import kotlin.properties.Delegates
+
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    private var typePokemonColor by Delegates.notNull<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
+        typePokemonColor = getColor(R.color.Poison)
+        binding.container.setBackgroundColor(typePokemonColor)
+        binding.arrowBack.setOnClickListener() {
+            val intent = Intent(this, ListActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(intent)
+        }
+
+
+
         setContentView(binding.root)
     }
 }
