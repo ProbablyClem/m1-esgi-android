9cf3523cb517e9fa6456d5e2f672800da4e02782
Lastoss <73163448+CooBsCa@users.noreply.github.com>
2023-07-25 14:22:26 +0200

    Add : - Fonts - Styles

--- app/src/main/java/com/example/pokeapp/DetailActivity.kt
+++ app/src/main/java/com/example/pokeapp/DetailActivity.kt
@@ -2,10 +2,13 @@ package com.example.pokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokeapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
