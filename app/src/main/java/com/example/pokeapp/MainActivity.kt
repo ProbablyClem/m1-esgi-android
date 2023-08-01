fc2f2973f2d6c0a5491816275d256fdf6a01e2db
Clément Guiton <clement.guiton.dev@gmail.com>
2023-08-01 22:37:19 +0200

    main activiy

--- app/src/main/java/com/example/pokeapp/MainActivity.kt
+++ app/src/main/java/com/example/pokeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.View
 import com.example.pokeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.searchBar.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                //Perform Code
+                search()
+                return@OnKeyListener true
+            }
+            false
+        })
         binding.searchButton.setOnClickListener {
-            val intent = Intent(this, ListActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
-            startActivity(intent)
+          search();
         }
 
         setContentView(binding.root)
 
+    }
+
+    fun search(){
+        val intent = Intent(this, ListActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+        startActivity(intent)
     }
 }
